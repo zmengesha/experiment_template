710bb02068431300c9bcb8f685f335278d1c713d
zmengesha <zamengesha@stanford.edu>
2018-04-25 11:24:35 -0700

    added experiment

---
+++ practice/js/stims.js
@@ -0,0 +1,19 @@
+var stimuli = [
+	{
+		lure: "mountain"
+	},
+	{
+		lure: "cup"
+	}
+];
+
+var mathproblems = [
+	{
+		problem: "4x + 6 = -10",
+		problem_answer: "-4"
+	},
+	{
+		problem: "x + 9 = 18 - 2x",
+		problem_answer: "3"
+	}
+];
